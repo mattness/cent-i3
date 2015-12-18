6083c1648cca9386e1704df8aafaaa1be71129f2
Matt Gollob <mattgollob@gmail.com>
2015-12-17 22:27:21 -0800

    idk... i3 can't find pango.h

--- build.sh
+++ build.sh
@@ -10,8 +10,9 @@ set -e
 #   docbook-utils-pdf libffi-devel libXext-devel \
 #   libX11-devel
 sudo yum install -y man vim curl git gcc gcc-c++ svn autoconf automake \
-  libtool make patch gperf cmake gettext bison libffi-devel \
-  libxml2-devel gtk-doc
+  libtool make patch gperf cmake gettext bison gtk-doc libffi-devel \
+  zlib-devel
+  # libxml2-devel gtk-doc
 
 # sudo yum groupinstall -y "X Window System"
 # sudo yum groupinstall -y "Desktop Platform Development"
@@ -71,7 +72,7 @@ git clone http://git.lighttpd.net/libev.git
 # curl -s http://dist.schmorp.de/libev/Attic/libev-4.19.tar.gz | tar zxf -
 git clone https://github.com/lloyd/yajl.git
 # http://github.com/lloyd/yajl/tarball/2.1.0
-svn co svn://vcs.exim.org/pcre/code/tags/pcre-8.35 pcre
+svn co svn://vcs.exim.org/pcre/code/tags/pcre-8.12 pcre
 git clone http://anongit.freedesktop.org/git/pixman.git
 # http://cairographics.org/releases/pixman-0.30.2.tar.gz
 git clone git://git.sv.nongnu.org/freetype/freetype2.git
@@ -96,23 +97,21 @@ git clone https://github.com/i3/i3lock.git
 
 ## Build an updated pkgconfig
 cd /src/pkg-config
-git checkout pkg-config-0.28
-./autogen.sh --with-internal-glib
-# ./configure --enable-static=yes --enable-shared=no --with-internal-glib
+git checkout pkg-config-0.25
+./autogen.sh
 make && sudo make install && hash -r
 
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig
 export ACLOCAL_PATH=/usr/local/share/aclocal-1.11:/usr/local/share/aclocal
-export ACLOCAL_FLAGS="-I /usr/local/share/aclocal"
+export ACLOCAL_FLAGS='-I /usr/local/share/aclocal'
 # export LD_LIBRARY_PATH=/usr/local/lib/:/usr/local/lib64/
 # export LD_RUN_PATH=/usr/local/lib/:/usr/local/lib64/
 
 # glib
 cd /src/glib
-git checkout 2.33.14
+git checkout 2.31.0
 export ACLOCAL="aclocal -I /usr/local/share/aclocal"
 ./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
 make && sudo make install
 
 # Macros
@@ -120,119 +119,122 @@ cd /src/macros
 git checkout util-macros-1.18.0
 export ACLOCAL="aclocal -I /usr/local/share/aclocal"
 ./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
 make && sudo make install
 
 # proto
-cd /src/proto
-git checkout 1.11
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/proto
+# git checkout 1.10
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
 # pthread-stubs
-cd /src/pthread-stubs
-git checkout 0.3
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/pthread-stubs
+# git checkout 0.3
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
 # xproto
-cd /src/xproto/
-git checkout xproto-7.0.28
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/xproto/
+# git checkout xproto-7.0.28
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
-# libXau
-cd /src/libXau/
-git checkout libXau-1.0.8
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# # libXau
+# cd /src/libXau/
+# git checkout libXau-1.0.8
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
+sudo yum install -y libXau-devel
+
+# XDMCP
+# sudo yum install -y libXdmcp-devel
 
 # libxcb
-cd /src/libxcb/
-git checkout 1.11
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/libxcb/
+# git checkout 1.10
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
+sudo yum install -y libxcb-devel
 
-# xcb-util
-cd /src/xcb-util
-git checkout 0.4.0
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# # xcb-util
+# cd /src/xcb-util
+# git checkout 0.4.0
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+#
+# make && sudo make install
+sudo yum install -y xcb-util-devel
+# sudo yum install -y xkeyboard-config{,-devel}
+# sudo yum install -y xorg-x11-xkb-utils{,-devel}
 
 # xkbcommon
-cd /src/libxkbcommon/
-git checkout xkbcommon-0.5.0
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/libxkbcommon/
+# git checkout xkbcommon-0.5.0
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
 # util-render
-cd /src/util-renderutil/
-git checkout 0.3.9
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/util-renderutil/
+# git checkout 0.3.9
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
 # util-image
-cd /src/util-image/
-git checkout 0.4.0
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/util-image/
+# git checkout 0.4.0
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+#
+# make && sudo make install
+sudo yum install -y xcb-util-image-devel
 
 # util-cursor
-cd /src/util-cursor/
-git checkout 0.1.2
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/util-cursor/
+# git checkout 0.0.99
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
-# util-wm
-cd /src/util-wm
-git checkout 0.3.8
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# # util-wm
+# cd /src/util-wm
+# git checkout 0.3.8
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+#
+# make && sudo make install
+# sudo yum install -y xcb-util-wm-devel
 
-# util-keysyms
-cd /src/util-keysyms
-git checkout 0.4.0
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# # util-keysyms
+# cd /src/util-keysyms
+# git checkout 0.4.0
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+#
+# make && sudo make install
+# sudo yum install -y xcb-util-keysyms-devel
 
 # libev
 # cd /src/libev-4.19
 cd /src/libev
-git checkout rel-4.19
+git checkout rel-4.11
 export ACLOCAL="aclocal -I /usr/local/share/aclocal"
 autoreconf -v --install && ./configure
-# ./configure --enable-static=yes --enable-shared=no
 make && sudo make install
+
+# ruby
+sudo yum install -y ruby
 
 # yajl
 cd /src/yajl
-git checkout 2.1.0
+git checkout 2.0.4
 ./configure
-# ./configure --enable-static=yes --enable-shared=no
 make && sudo make install
 
 # pcre
@@ -240,151 +242,146 @@ cd /src/pcre
 export ACLOCAL="aclocal -I /usr/local/share/aclocal"
 ./autogen.sh
 ./configure && make && sudo make install
-# ./configure --enable-static=yes --enable-shared=no && make && sudo make install
 
-# pixman
-cd /src/pixman
-git checkout pixman-0.30.2
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# # pixman
+# cd /src/pixman
+# git checkout pixman-0.30.2
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
-# png
-cd /src/libpng
-git checkout v1.2.55
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh && ./configure
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# # png
+# cd /src/libpng
+# git checkout v1.2.55
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh && ./configure
+# make && sudo make install
 
-# freetype2
-cd /src/freetype2
-git checkout VER-2-6-2
-ACLOCAL='' ./autogen.sh && ./configure
-# ./autogen.sh && ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# # freetype2
+# cd /src/freetype2
+# git checkout VER-2-6-2
+# ACLOCAL='' ./autogen.sh && ./configure
+# # ./autogen.sh && ./configure --enable-static=yes --enable-shared=no
+# make && sudo make install
 
-# fontconfig
-cd /src/fontconfig
-git checkout 2.10.95
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh --enable-libxml2 --disable-docs
-# ./configure --enable-static=yes --enable-shared=no --disable-docs --enable-libxml2
-make && sudo make install
+# # fontconfig
+# cd /src/fontconfig
+# git checkout 2.10.95
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh --enable-libxml2 --disable-docs --disable-docs --enable-libxml2
+# make && sudo make install
 
-# cairo
-cd /src/cairo
-git checkout 1.14.4
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# # cairo
+# cd /src/cairo
+# git checkout 1.14.4
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
-# harfbuzz
-cd /src/harfbuzz-1.1.1
-# ./configure --enable-static=yes --enable-shared=no && make && sudo make install
-./configure && make && sudo make install
+# # harfbuzz
+# cd /src/harfbuzz-1.1.1 && make && sudo make install
+# ./configure && make && sudo make install
 
-# pango
+# # pango
 cd /src/pango
-git checkout 1.36.8
+git checkout 1.30.0
 export ACLOCAL="aclocal -I /usr/local/share/aclocal"
 ./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-patch -p 1 <<'EOH'
-diff --git a/tests/markup-parse.c b/tests/markup-parse.c
-index e9cb6a5..04521d0 100644
---- a/tests/markup-parse.c
-+++ b/tests/markup-parse.c
-@@ -228,7 +228,7 @@ main (int argc, char *argv[])
+# patch -p 1 <<'EOH'
+# diff --git a/tests/markup-parse.c b/tests/markup-parse.c
+# index e9cb6a5..04521d0 100644
+# --- a/tests/markup-parse.c
+# +++ b/tests/markup-parse.c
+# @@ -228,7 +228,7 @@ main (int argc, char *argv[])
 
-       return 0;
-     }
--
-+/*
-   path = g_test_build_filename (G_TEST_DIST, "markups", NULL);
-   dir = g_dir_open (path, 0, &error);
-   g_free (path);
-@@ -244,6 +244,6 @@ main (int argc, char *argv[])
-       g_free (path);
-     }
-   g_dir_close (dir);
--
-+*/
-   return g_test_run ();
- }
-diff --git a/tests/test-layout.c b/tests/test-layout.c
-index fffc963..514a163 100644
---- a/tests/test-layout.c
-+++ b/tests/test-layout.c
-@@ -522,7 +522,7 @@ main (int argc, char *argv[])
+#        return 0;
+#      }
+# -
+# +/*
+#    path = g_test_build_filename (G_TEST_DIST, "markups", NULL);
+#    dir = g_dir_open (path, 0, &error);
+#    g_free (path);
+# @@ -244,6 +244,6 @@ main (int argc, char *argv[])
+#        g_free (path);
+#      }
+#    g_dir_close (dir);
+# -
+# +*/
+#    return g_test_run ();
+#  }
+# diff --git a/tests/test-layout.c b/tests/test-layout.c
+# index fffc963..514a163 100644
+# --- a/tests/test-layout.c
+# +++ b/tests/test-layout.c
+# @@ -522,7 +522,7 @@ main (int argc, char *argv[])
 
-       return 0;
-     }
--
-+/*
-   path = g_test_build_filename (G_TEST_DIST, "layouts", NULL);
-   dir = g_dir_open (path, 0, &error);
-   g_free (path);
-@@ -538,6 +538,6 @@ main (int argc, char *argv[])
-       g_free (path);
-     }
-   g_dir_close (dir);
--
-+*/
-   return g_test_run ();
- }
-EOH
+#        return 0;
+#      }
+# -
+# +/*
+#    path = g_test_build_filename (G_TEST_DIST, "layouts", NULL);
+#    dir = g_dir_open (path, 0, &error);
+#    g_free (path);
+# @@ -538,6 +538,6 @@ main (int argc, char *argv[])
+#        g_free (path);
+#      }
+#    g_dir_close (dir);
+# -
+# +*/
+#    return g_test_run ();
+#  }
+# EOH
 make && sudo make install
 
 # libxtrans
-cd /src/libxtrans
-git checkout xtrans-1.3.5
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/libxtrans
+# git checkout xtrans-1.3.5
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
-# xextproto
-cd /src/xextproto
-git checkout xextproto-7.3.0
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-make && sudo make install
+# # xextproto
+# cd /src/xextproto
+# git checkout xextproto-7.3.0
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
-# kbproto
-cd /src/kbproto
-git checkout kbproto-1.0.7
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-make && sudo make install
+# # kbproto
+# cd /src/kbproto
+# git checkout kbproto-1.0.7
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
 
-# inputproto
-cd /src/inputproto
-git checkout inputproto-2.3.1
-./autogen.sh
-make && sudo make install
+# # inputproto
+# cd /src/inputproto
+# git checkout inputproto-2.3.1
+# ./autogen.sh
+# make && sudo make install
 
 # libX11
-cd /src/libX11
-git checkout libX11-1.6.3
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/libX11
+# git checkout libX11-1.6.3
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
+# sudo yum install -y libX11-devel
 
 # libsn
-cd /src/startup-notification
-git checkout STARTUP_NOTIFICATION_0_12
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-# ./configure --enable-static=yes --enable-shared=no
-make && sudo make install
+# cd /src/startup-notification
+# git checkout STARTUP_NOTIFICATION_0_12
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# ./autogen.sh
+# make && sudo make install
+sudo yum install -y startup-notification-devel
+
+# util-keysyms
+sudo yum install -y xcb-util-keysyms-devel
 
 # i3
 cd /src/i3
-git checkout 4.11
+git checkout 4.8
 # make i3_LIBS='-L/usr/local/lib -lxkbcommon -lxcb -lyajl_s -lev -lxkbcommon-x11 -lxcb-keysyms -lstartup-notification-1 -lxcb-icccm -lxcb-util -lxcb-xkb -lxcb-randr -lxcb-cursor -lxcb-xinerama -lm -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lcairo -lXau -lX11-xcb -lxcb-render -lxcb-render-util -lxcb-image -lpangoft2-1.0 -lfontconfig -lfreetype -lgmodule-2.0 -lffi -lpixman-1 -lxcb-shm -lxml2 -lglib-2.0' i3_config_wizard_LIBS='-L/usr/local/lib -lxcb -lxcb-keysyms -lxkbcommon -lxkbcommon-x11 -lxcb-util -lpangocairo-1.0 -lpango-1.0 -lcairo -lgobject-2.0 -lxcb-xkb -lXau -lpangoft2-1.0 -lfontconfig -lfreetype -lgmodule-2.0 -lpixman-1 -lxcb-render -lxcb-shm -lglib-2.0 -lpthread -lffi -lxml2 -lrt' i3_msg_LIBS='-L/usr/local/lib -lyajl_s -lxcb -lXau -lxcb-util' i3_input_LIBS='-L/usr/local/lib -lxcb -lxcb-keysyms -lxcb-util -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lcairo -lXau -lpangoft2-1.0 -lfontconfig -lfreetype -lgmodule-2.0 -lpixman-1 -lxcb-render -lxcb-shm -lglib-2.0 -lffi -lpthread -lxml2 -lrt' i3_nagbar_LIBS='-L/usr/local/lib -lxcb -lxcb-randr -lxcb-util -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lcairo -lXau -lpangoft2-1.0 -lfontconfig -lfreetype -lgmodule-2.0 -lpixman-1 -lxcb-render -lxcb-shm -lglib-2.0 -lffi -lpthread -lxml2 -lrt' i3bar_LIBS='-L/usr/local/lib -lxcb -lxcb-randr -lxcb-util -lpangocairo-1.0 -lpango-1.0 -lgobject-2.0 -lcairo -lXau -lpangoft2-1.0 -lfontconfig -lfreetype -lgmodule-2.0 -lpixman-1 -lxcb-render -lxcb-shm -lev -lyajl_s -lxcb-xkb -lXau -lglib-2.0 -lffi -lpthread -lxml2 -lrt' i3_dump_log_LIBS='-L/usr/local/lib -lxcb -lxcb-util -lXau' && sudo make install
 make && sudo make install
 
@@ -397,3 +394,16 @@ make && sudo make install
 # cd /src/i3lock
 # git checkout 2.7
 # make PREFIX=/opt/i3 && sudo make install PREFIX=/opt/i3
+
+sudo yum install -y epel-release
+sudo yum groupinstall -y 'X Window System'
+sudo yum install -y xorg-x11-xdm rxvt-unicode-256color dmenu
+
+sudo tee /usr/local/bin/i3 <<'EOH' > /dev/null
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/usr/local/lib
+/usr/bin/i3-with-shmlog
+EOH
+
+sudo chmod +x /usr/local/bin/i3
