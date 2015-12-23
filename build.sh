0e351f8f3c0ca847726d44410721f37e33fc199f
Matt Gollob <mattgollob@gmail.com>
2015-12-23 01:44:08 -0800

    Bam! Smallest script I could make, statically-linked

--- build.sh
+++ build.sh
@@ -8,27 +8,19 @@ sudo yum install -y man vim curl git gcc gcc-c++ svn autoconf automake \
   zlib-devel libXau-devel libxcb-devel xcb-util-devel xcb-util-image-devel \
   ruby freetype-devel fontconfig-devel libpng-devel pixman-devel \
   startup-notification-devel xcb-util-keysyms-devel xcb-util-wm-devel \
-  pcre-devel libconfuse-devel alsa-lib-devel wireless-tools-devel asciidoc
+  pcre-devel alsa-lib-devel wireless-tools-devel asciidoc \
+  xorg-x11-util-macros-1.17 epel-release
+
+sudo yum install -y libev-devel libconfuse-devel
 
 sudo mkdir -p -m 775 /src && sudo chown root:vagrant /src && cd $_
 
 # Build i3
 cd /src
-git clone http://anongit.freedesktop.org/git/pkg-config.git
-# curl -s http://pkgconfig.freedesktop.org/releases/pkg-config-0.25.tar.gz | tar zxf -
-git clone https://git.gnome.org/browse/glib
-# curl -s http://ftp.gnome.org/pub/gnome/sources/glib/2.31/glib-2.31.0.tar.xz
-git clone http://anongit.freedesktop.org/git/xorg/util/macros.git
-# curl -s http://xorg.freedesktop.org/releases/individual/util/util-macros-1.18.0.tar.gz | tar zxf -
 git clone http://anongit.freedesktop.org/git/xcb/util-cursor.git --recursive
 # http://xcb.freedesktop.org/dist/xcb-util-cursor-0.1.0.tar.gz
-git clone http://git.lighttpd.net/libev.git
-# curl -s http://dist.schmorp.de/libev/Attic/libev-4.11.tar.gz | tar zxf -
 git clone https://github.com/lloyd/yajl.git
 # curl -s http://github.com/lloyd/yajl/tarball/2.0.4 | tar zxf -
-git clone http://anongit.freedesktop.org/git/cairo
-# http://cairographics.org/releases/cairo-1.12.2.tar.xz
-git clone git://git.gnome.org/pango
 git clone https://github.com/i3/i3.git
 # curl -s https://github.com/i3/i3/archive/4.8.tar.gz | tar zxf -
 git clone https://github.com/i3/i3status.git
@@ -36,43 +28,13 @@ git clone https://github.com/i3/i3status.git
 git clone https://github.com/i3/i3lock.git
 # curl -s https://github.com/i3/i3lock/archive/2.6.tar.gz | tar zxf -
 
-## Build an updated pkgconfig
-cd /src/pkg-config
-git checkout pkg-config-0.25
-./autogen.sh
-make && sudo make install && hash -r
-
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig
-export ACLOCAL_PATH=/usr/local/share/aclocal-1.11:/usr/local/share/aclocal
-export ACLOCAL_FLAGS='-I /usr/local/share/aclocal'
-
-# glib
-cd /src/glib
-git checkout 2.31.0
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-make && sudo make install
-
-# Macros
-cd /src/macros
-git checkout util-macros-1.18.0
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-make && sudo make install
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig
 
 # util-cursor
 cd /src/util-cursor/
 git checkout 0.1.0
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
+# export ACLOCAL="aclocal -I /usr/local/share/aclocal"
 ./autogen.sh
-make && sudo make install
-
-# libev
-# cd /src/libev-4.19
-cd /src/libev
-git checkout rel-4.11
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-autoreconf -v --install && ./configure
 make && sudo make install
 
 # yajl
@@ -81,24 +43,35 @@ git checkout 2.0.4
 ./configure
 make && sudo make install
 
-# cairo
-cd /src/cairo
-git checkout 1.12.2
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-make && sudo make install
-
-# pango
-cd /src/pango
-git checkout 1.30.0
-export ACLOCAL="aclocal -I /usr/local/share/aclocal"
-./autogen.sh
-make && sudo make install
-
 # i3
 cd /src/i3
 git checkout 4.8
-make && sudo make install
+patch -p1 <<'EOH'
+diff --git a/common.mk b/common.mk
+index b086bc8..fcf9dbf 100644
+--- a/common.mk
++++ b/common.mk
+@@ -133,11 +133,11 @@ LIBSN_CFLAGS := $(call cflags_for_lib, libstartup-notification-1.0)
+ LIBSN_LIBS   := $(call ldflags_for_lib, libstartup-notification-1.0,startup-notification-1)
+ 
+ # Pango
+-PANGO_CFLAGS := $(call cflags_for_lib, cairo)
+-PANGO_CFLAGS += $(call cflags_for_lib, pangocairo)
+-I3_CPPFLAGS  += -DPANGO_SUPPORT=1
+-PANGO_LIBS   := $(call ldflags_for_lib, cairo)
+-PANGO_LIBS   += $(call ldflags_for_lib, pangocairo)
++# PANGO_CFLAGS := $(call cflags_for_lib, cairo)
++# PANGO_CFLAGS += $(call cflags_for_lib, pangocairo)
++# I3_CPPFLAGS  += -DPANGO_SUPPORT=1
++# PANGO_LIBS   := $(call ldflags_for_lib, cairo)
++# PANGO_LIBS   += $(call ldflags_for_lib, pangocairo)
+ 
+ # libi3
+ LIBS = -L$(TOPDIR) -li3 -lm
+EOH
+make YAJL_LIBS='-lyajl_s' \
+  XCURSOR_LIBS="-Wl,-Bstatic -L/usr/local/lib -lxcb-cursor -Wl,-Bdynamic -lxcb-render-util -lxcb-image -lxcb-render -lxcb-shm -lxcb -lXau" \
+  && sudo make install
 
 # i3Status
 cd /src/i3status
@@ -114,28 +87,10 @@ make && sudo make install
 
 # Extra stuff for testing
 
-sudo yum install -y epel-release
 sudo yum groupinstall -y 'X Window System' 'Fonts'
 sudo yum install -y gdm rxvt-unicode-256color dmenu
 sudo yum install -y kernel-devel
 
-sudo tee /usr/local/bin/i3 <<'EOH' > /dev/null
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/usr/local/lib
-/usr/bin/i3
-EOH
-
-sudo tee /usr/local/bin/i3-with-shmlog <<'EOH' > /dev/null
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/usr/local/lib
-/usr/bin/i3-with-shmlog
-EOH
-
-sudo chmod +x /usr/local/bin/i3{,-with-shmlog}
-sudo sed -i 's%Exec=i3%Exec=/usr/local/bin/i3%' /usr/share/xsessions/i3.desktop
-sudo sed -i 's%Exec=i3%Exec=/usr/local/bin/i3%' /usr/share/xsessions/i3-with-shmlog.desktop
 sudo sed -i 's/id:3/id:5/' /etc/inittab
 
 git clone git://github.com/mattness/dotfiles.git $HOME/.dotfiles && ${_}/install <<<'B'
